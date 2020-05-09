ce16bb61b57eabac6cf07b47c227cbdbea980241
space2pacman <space2pacman@gmail.com>
2020-05-09 18:52:50 +0300

    refactoring XOR

--- util/XOR.js
+++ util/XOR.js
@@ -1,62 +1,64 @@
-function XOR(key) {
-	this.decryptKey = new Buffer.from(key);
-	this.encryptKey = new Buffer.from(key);
-}
-
-XOR.prototype.decrypt = function(data) {
-	let temp = 0;
-	let count = 0;
-		  
-	for (let i = 0; i < data.length; ++i) {
-		if(count == 8) count = 0;
-		    
-		let temp2 = data[i] & 0xff;
-		data[i] = (temp2 ^ (this.decryptKey[count]) ^ temp);
-		temp = temp2;
-		count++;
+class XOR {
+	constructor(key) {
+		this.decryptKey = new Buffer.from(key);
+		this.encryptKey = new Buffer.from(key);
 	}
 
-	let old = this.decryptKey[0] & 0xff;
-	old |= this.decryptKey[1] << 8 & 0xff00;
-	old |= this.decryptKey[2] << 0x10 & 0xff0000;
-	old |= this.decryptKey[3] << 0x18 & 0xff000000;
-		
-	old += data.length;
-		
-	this.decryptKey[0] = (old & 0xff);
-	this.decryptKey[1] = (old >> 0x08 & 0xff);
-	this.decryptKey[2] = (old >> 0x10 & 0xff);
-	this.decryptKey[3] = (old >> 0x18 & 0xff);
-	
-	return data;
-}
+	decrypt(data) {
+		let temp = 0;
+		let count = 0;
+			  
+		for (let i = 0; i < data.length; ++i) {
+			if(count == 8) count = 0;
+			    
+			let temp2 = data[i] & 0xff;
+			data[i] = (temp2 ^ (this.decryptKey[count]) ^ temp);
+			temp = temp2;
+			count++;
+		}
 
-XOR.prototype.encrypt = function(data) {
-	let temp = 0;
-	let count = 0;
-		  
-	for (let i = 0; i < data.length; ++i) {
-		if(count == 8) count = 0;
-		    
-		let temp2 = data[i] & 0xff;
-		data[i] = (temp2 ^ (this.encryptKey[count]) ^ temp);
-		temp = data[i];
-		count++;
+		let old = this.decryptKey[0] & 0xff;
+		old |= this.decryptKey[1] << 8 & 0xff00;
+		old |= this.decryptKey[2] << 0x10 & 0xff0000;
+		old |= this.decryptKey[3] << 0x18 & 0xff000000;
+			
+		old += data.length;
+			
+		this.decryptKey[0] = (old & 0xff);
+		this.decryptKey[1] = (old >> 0x08 & 0xff);
+		this.decryptKey[2] = (old >> 0x10 & 0xff);
+		this.decryptKey[3] = (old >> 0x18 & 0xff);
+		
+		return data;
 	}
 
-	let old = this.encryptKey[0] & 0xff;
-	old |= this.encryptKey[1] << 8 & 0xff00;
-	old |= this.encryptKey[2] << 0x10 & 0xff0000;
-	old |= this.encryptKey[3] << 0x18 & 0xff000000;
-		
-	old += data.length;
-		
-	this.encryptKey[0] = (old & 0xff);
-	this.encryptKey[1] = (old >> 0x08 & 0xff);
-	this.encryptKey[2] = (old >> 0x10 & 0xff);
-	this.encryptKey[3] = (old >> 0x18 & 0xff);
+	encrypt(data) {
+		let temp = 0;
+		let count = 0;
+			  
+		for (let i = 0; i < data.length; ++i) {
+			if(count == 8) count = 0;
+			    
+			let temp2 = data[i] & 0xff;
+			data[i] = (temp2 ^ (this.encryptKey[count]) ^ temp);
+			temp = data[i];
+			count++;
+		}
 
-	return data;
+		let old = this.encryptKey[0] & 0xff;
+		old |= this.encryptKey[1] << 8 & 0xff00;
+		old |= this.encryptKey[2] << 0x10 & 0xff0000;
+		old |= this.encryptKey[3] << 0x18 & 0xff000000;
+			
+		old += data.length;
+			
+		this.encryptKey[0] = (old & 0xff);
+		this.encryptKey[1] = (old >> 0x08 & 0xff);
+		this.encryptKey[2] = (old >> 0x10 & 0xff);
+		this.encryptKey[3] = (old >> 0x18 & 0xff);
+
+		return data;
+	}
 }
 
 module.exports = XOR;
