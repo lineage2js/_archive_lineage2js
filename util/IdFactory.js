2ebdb0dedf2d779de9ae8131cda24c38adc69d36
space2pacman <space2pacman@gmail.com>
2020-05-09 18:54:28 +0300

    refactoring IdFactory

--- util/IdFactory.js
+++ util/IdFactory.js
@@ -1,37 +1,39 @@
 let file = require("fs");
 
-function IdFactory(path) {
-	this._FIRST_OID = 0x10000000;
-	this._path = path;
-	this._data = this.loadCurrentState();
-	
-	if(this._data === false) {
-		this._data = { id: this._FIRST_OID };
-		this.saveCurrentState();
+class IdFactory {
+	constructor(path) {
+		this._FIRST_OID = 0x10000000;
+		this._path = path;
 		this._data = this.loadCurrentState();
+		
+		if(this._data === false) {
+			this._data = { id: this._FIRST_OID };
+			this.saveCurrentState();
+			this._data = this.loadCurrentState();
+		}
 	}
-}
 
-IdFactory.prototype.getNextId = function() {
-	let id = this._data.id;
-	
-	this._data.id++;
-	this.saveCurrentState();
+	getNextId() {
+		let id = this._data.id;
+		
+		this._data.id++;
+		this.saveCurrentState();
 
-	return id;
-}
+		return id;
+	}
 
-IdFactory.prototype.saveCurrentState = function() {
-	file.writeFileSync(this._path, JSON.stringify(this._data));
-}
+	saveCurrentState() {
+		file.writeFileSync(this._path, JSON.stringify(this._data));
+	}
 
-IdFactory.prototype.loadCurrentState = function() {
-	let data = file.readFileSync(this._path, "utf-8");
+	loadCurrentState() {
+		let data = file.readFileSync(this._path, "utf-8");
 
-	if(data.length === 0) {
-		return false
-	} else {
-		return JSON.parse(data);
+		if(data.length === 0) {
+			return false
+		} else {
+			return JSON.parse(data);
+		}
 	}
 }
 
